6c32ce126637a00dcbbc6f632991a2f145f24abd
David Johnsonn <djohnso08@gmail.com>
2022-02-06 16:34:02 -0700

    add for loop and lowercase variable

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,43 @@
 // let wordSplit = word.split();
 // if {word[0] === vowelArray.foreach()};
 // const vowelArray = ['a', 'e','i','o','u'];
+//  for(i=0,i < vowellArray.length, i++)
+
+// const vowelArray = ['a', 'e','i','o','u'];
+// let word = vowelArray.toLowerCase();
+// let changedWord = (word[0] + "way");
+// if (word === "a") {
+// return changedWord;
 
 
 // Business Logic
-function pigLatin(word)  {
-  // let vowel = "a";
-  const vowelArray = ['a', 'e','i','o','u'];
-  let word = vowelArray.toLowerCase();
-  let changedWord = (word[0] + "way");
-  if (word === "a") {
-  return changedWord;
+function pigLatin(str)  {
+  console.log(str, typeof str);
+  let lowercase = str.toLowerCase();
+  let array = lowercase.split("");
+  console.log(array);
+  let vowels = ['a','e','i','o','u'];
+  for (let i = 0; i < vowels.length; i++) {
+    if (array[0] != vowels[i]) {
+      return false;
+  }
+  else return true;
   }
 };
 
- pigLatin(eAt);
+// function dnaPatternDetector(dnaSequence, pattern) {
+//   for (let i = 0; i < dnaSequence.length; i +=1) {
+//     if (dnaSequence[i] === pattern) {
+//       return true;
+//     };
+//     console.log("Looped!");
+//   }
+//   return false;
+// }
+// const sequence = ["X", "A", "Y", "M", "D"];
+// const pattern1 = "A";
+// const pattern2 = "Z";
+
+pigLatin()
 
 // Utility Logic
