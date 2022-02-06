3cd94f8d7cee9e7c8b762085f3f564ed77a1ff96
David Johnsonn <djohnso08@gmail.com>
2022-02-06 12:51:18 -0700

    add pigLatin function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// UI Logic
+let wordSplit = word.split();
+if {word[0] === vowelArray.foreach()};
+const vowelArray = ['a', 'e','i','o','u'];
+
+
+// Business Logic
+pigLatin(word) {
+  let lowercase = word.toLowerCase();
+  let vowel = "a";
+  pigLatin = (vowel + "way");
+}
+
+
+
+// Utility Logic
